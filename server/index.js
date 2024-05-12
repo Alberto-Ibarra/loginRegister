b4fc23f20fd91f1770675c0a4c57f2b6e2c9bf9c
Alberto-Ibarra <alberto.e.ibarra@gmail.com>
2024-05-12 16:34:54 -0500

    connected mongodb

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const cors = require('cors')
-const mongoose = require('mongoose'); // Corrected import statement
-const authRoutes = require('./routes/authRoutes'); // Corrected path
+const mongoose = require('mongoose'); 
+const authRoutes = require('./routes/authRoutes'); 
 
 
 const app = express()
 const port = 8000
 app.use(cors()); // Apply CORS middleware globally
 app.use(express.json()); // Parse JSON bodies
+
+//database connection
+mongoose.connect(process.env.MONGOURI)
+    .then(() => console.log('connected to mongodb'))
+    .catch((err) => console.log('database not connected', err))
 
 app.use('/', authRoutes)
 
